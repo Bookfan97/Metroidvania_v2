3f3431642baa32d5f2c24172215078a5585f5014
Noah <sheltielover432@gmail.com>
2020-01-01 17:52:16 -0500

    Setting up player

--- Udemy_Platform/Assets/Scripts/PlayerController.cs
+++ Udemy_Platform/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D theRB;
+    public float jumpForce;
+    private bool isGrounded, canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+            else
+            {
+                if(canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
     }
 }
