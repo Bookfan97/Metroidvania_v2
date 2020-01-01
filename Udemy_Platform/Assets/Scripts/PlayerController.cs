9ba8e4fb5227821a8c5e613ff89f9befbb14a427
Noah <sheltielover432@gmail.com>
2020-01-01 18:27:22 -0500

    Animation Controller

--- Udemy_Platform/Assets/Scripts/PlayerController.cs
+++ Udemy_Platform/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,13 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded, canDoubleJump;
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
+    private Animator anim;
+    private SpriteRenderer theSR;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -40,5 +43,15 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
+        if(theRB.velocity.x <0)
+        {
+            theSR.flipX = true;
+        }
+        else if(theRB.velocity.x > 0)
+        {
+            theSR.flipX = false;
+        }
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
     }
 }
