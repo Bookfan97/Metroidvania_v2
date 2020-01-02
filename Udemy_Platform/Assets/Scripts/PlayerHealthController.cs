2e8f058210e58126c63b939bee54c2bd58eec2b8
Noah <sheltielover432@gmail.com>
2020-01-01 21:07:35 -0500

    Invincibilty

--- Udemy_Platform/Assets/Scripts/PlayerHealthController.cs
+++ Udemy_Platform/Assets/Scripts/PlayerHealthController.cs
@@ -6,6 +6,9 @@ public class PlayerHealthController : MonoBehaviour
 {
     public static PlayerHealthController instance;
     public int currentHealth, maxHealth;
+    public float invincibleLength;
+    private float invincibleCounter;
+    private SpriteRenderer theSR;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -14,22 +17,38 @@ public class PlayerHealthController : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(invincibleCounter>0)
+        {
+            invincibleCounter -= Time.deltaTime;
+            if(invincibleCounter<=0)
+            {
+                theSR.color = new Color(theSR.color.r, theSR.color.g, theSR.color.b, 1f);
+            }
+        }
     }
 
     public void DealDamage()
     {
-        currentHealth -= 1;
-        if(currentHealth <= 0)
+        if(invincibleCounter <=0)
         {
-            currentHealth = 0;
-            gameObject.SetActive(false);
+            currentHealth -= 1;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invincibleCounter = invincibleLength;
+                theSR.color= new Color(theSR.color.r, theSR.color.g, theSR.color.b, 0.5f);
+            }
+            UIController.instance.UpdateHealthDisplay();
         }
-        UIController.instance.UpdateHealthDisplay();
     }
 }
